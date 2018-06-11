376e7e5a907ae72953d7bce8a4f2168539747178
Nigorjeanluc <nigorjeanluc@gmail.com>
2018-06-11 10:48:14 +0200

    hdjdj

--- admin/pages/user/human.php
+++ admin/pages/user/human.php
@@ -71,7 +71,6 @@
                             <h3 class="box-title">Latest Intrusions</h3>
 
                             <div class="box-tools pull-right">
-                                <span class="label label-danger">8 New Members</span>
                                 <!--<button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
                                 </button>
                                 <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i>
